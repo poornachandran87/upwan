48f59bb0709bac1e2d68c66d55e839b2813cd4c7
poornachandran872 <chandru08july2000@gmail.com>
2024-03-08 23:59:22 +0530

    image upload bug

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -13,6 +13,8 @@ const upload = multer({storage: multer.diskStorage({
         cb(null, file.originalname)
     }
 }) })
+const p = path.join( __dirname,'..' , 'uploads/product' )
+console.log(p);
 
 router.route('/products').get(getProducts)
 //router.route('/products').get(isAuthenticatedUser,getProducts)
